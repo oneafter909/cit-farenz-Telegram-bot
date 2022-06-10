51f29e3e989d20c471b766a95533794f02a96612
Vincenzo Talarico <56183806+oneafter909@users.noreply.github.com>
2022-06-10 23:38:25 +0200

    Update MakeImage.py

--- MakeImage.py
+++ MakeImage.py
@@ -10,7 +10,6 @@ title_font = ImageFont.truetype('ED.TTF', font_size)
 title_text = sys.argv[1]
 textRectangle_width = 1965
 textRectangle_height = 1217
-#LUNGHEZZA MAX 1079
 def text_wrap(text, font, max_width):
         lines = []
         if font.getsize(text)[0]  <= max_width:
@@ -41,7 +40,7 @@ edit_width, edit_height = button_draw.textsize(testoFinale, title_font)
 if(edit_height <= 1000):
     button_draw.text((1000, textRectangle_height/2), testoFinale + citMarco, font=title_font, anchor="rm", fill="black", align='right')
 else:
-    #FUORI DAI LIMITI
+    #OUT OF BOX
     button_draw.text((1000, textRectangle_height/2), "Inserisci testi corti.", font=title_font, anchor="rm", fill="black", align='right')
 
 my_image.paste(button_image, (881, 0))
